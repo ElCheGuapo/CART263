a53da383e510998d8644f867d4be36bb492d4d30
Anon_01 <57970996+ElCheGuapo@users.noreply.github.com>
2022-02-09 12:30:50 -0500

    0.1

---
+++ Projects/project1/js/sketch.js
@@ -0,0 +1,44 @@
+/**
+Hotline Deadpool
+Author Name
+This is a template. You must fill in the title,
+author, and this description to match your project!
+*/
+
+"use strict";
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+function mousePressed() {
+  if (keyCode === 87) {
+    console.log("forward");
+  } else if (keyCode === 65) {
+    console.log("left");
+  } else if (keyCode === 83) {
+    console.log("backward");
+  } else if (keyCode === 68) {
+    console.log("right");
+  }
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
